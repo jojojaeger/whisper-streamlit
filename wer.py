2c6bdab4a3c9e72a8276abdd1a8e39f0e2798b1c
jojojaeger <65357373+jojojaeger@users.noreply.github.com>
2023-03-08 10:24:13 +0100

    Update wer.py

--- wer.py
+++ wer.py
@@ -13,14 +13,12 @@ def getText(filename):
         fullText.append(para.text)
     return ' '.join(fullText)
 
-
 def transformation(text):
     text = jiwer.RemovePunctuation()(text)
     text = jiwer.ToLowerCase()(text)
     text = jiwer.RemoveWhiteSpace(replace_by_space=True)(text)
     text = jiwer.RemoveMultipleSpaces()(text)
     return text
-
 
 ground_truth = transformation(getText(sys.argv[1]))
 
@@ -46,20 +44,5 @@ process = Popen(args, stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
 
 # Print the output to the console
-
 outtext = stdout.decode('latin-1')
-
-# Sperate strings for each section (not tested yet)
-outtext = outtext.split("INSERTIONS:")[0]
-insertions, rest = outtext.split("DELETIONS:")
-deletions, rest = rest.split("SUBSTITUTIONS:")
-substitutions, wer_info = rest.split("Sentence Count:")
-
-# TODO: write each string to a text file
-with open('result.txt', 'w', encoding="UTF-8") as f:
-    f.write(outtext)
-
-csv = pd.read_csv("result.txt", sep=" ")
-csv.to_csv(r'result.csv', index=None)
-#print('stdout:', stdout.decode('latin-1'))
-#print('stderr:', stderr.decode())
+print(outtext)
